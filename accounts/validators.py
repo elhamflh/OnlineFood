89b6bc77e61b80d9418eee25e5bf8a9e05643d60
elHam <elhamii.1512@gmail.com>
2023-10-26 00:11:56 +0330

    vendor-resturant page

---
+++ accounts/validators.py
@@ -0,0 +1,11 @@
+from django.core.exceptions import ValidationError
+import os
+
+def allow_only_images_validator(value):
+    ext = os.path.splitext(value.name)[1]   #cover-image.jpg
+    print(ext)
+    
+    valied_extentions = ['.jpg', '.png', '.jpeg',]
+    if not ext.lower() in valied_extentions:
+        raise ValidationError('Unsupported file extention. Allowed extentions:' +str(valied_extentions))
+
